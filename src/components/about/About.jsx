3a968fd9596003ad44596a6ed53b1a459a24970d
Deva9754 <deva.sahu2001@gmail.com>
2024-06-09 20:18:36 +0530

    added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,10 @@ import image from "../../assets/aboutImage.png";
 
 const About = () => {
   return (
-    <div className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+    <div
+      id="About"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
       <div className=" px-8">
         <h2 className=" px-14 text-4xl font-bold inline border-b-4 border-gray-500">
           About
@@ -14,8 +17,8 @@ const About = () => {
       <div className="content">
         <img className="about-image" src={image} alt="image_about" />
         <img src={cursor} alt="" />
-        <div className="about-item-text">
-          <h3>Frontend Developer</h3>
+        <div className="about-item-text aboutItem">
+          <h3 className=" font-bold">Frontend Developer</h3>
           <p>
             I`m a frontend developer with experience in building and optimized
             sites.
