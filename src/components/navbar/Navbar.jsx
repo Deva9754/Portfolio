68f7c025b33176044573de0d958f9a87a1d0cfa6
Deva9754 <deva.sahu2001@gmail.com>
2024-06-11 01:34:08 +0530

    wrapped inside container

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import menuclose from "../../assets/menu_close.svg";
 
 import { useRef, useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import Container from "../container/Container";
 
 const Navbar = () => {
   const [image, setImage] = useState("home");
@@ -16,53 +17,66 @@ const Navbar = () => {
     menuRef.current.style.width = "0";
   };
   return (
-    <div className="navbar">
-      {/* <img src={logo} alt="Logo" /> */}
-      <img src={menuopen} onClick={openMenu} alt="" className="nav-mob-open" />
-      <ul ref={menuRef} className="nav-menu">
-        <img
-          src={menuclose}
-          onClick={closeMenu}
-          alt=""
-          className="nav-mob-close"
-        />
-        <li>
-          <p onClick={() => setImage("home")}>Home</p>
-          {image === "home" ? <img src={abc} alt="" /> : <></>}
-        </li>
-        <li>
-          <AnchorLink className="anchor-link" offset={50} href="#experience">
-            <p onClick={() => setImage("experince")}>Experience</p>
-          </AnchorLink>
-          {image === "experience" ? <img src={abc} alt="" /> : <></>}
-        </li>
-        {/* <li>
+    <Container>
+      <div>
+        <div className="navbar">
+          {/* <img src={logo} alt="Logo" /> */}
+          <img
+            src={menuopen}
+            onClick={openMenu}
+            alt=""
+            className="nav-mob-open"
+          />
+          <ul ref={menuRef} className="nav-menu">
+            <img
+              src={menuclose}
+              onClick={closeMenu}
+              alt=""
+              className="nav-mob-close"
+            />
+            <li>
+              <p onClick={() => setImage("home")}>Home</p>
+              {image === "home" ? <img src={abc} alt="" /> : <></>}
+            </li>
+            <li>
+              <AnchorLink
+                className="anchor-link"
+                offset={50}
+                href="#experience"
+              >
+                <p onClick={() => setImage("experince")}>Experience</p>
+              </AnchorLink>
+              {image === "experience" ? <img src={abc} alt="" /> : <></>}
+            </li>
+            {/* <li>
           <AnchorLink className="anchor-link " offset={50} href="#About">
             <p onClick={() => setImage("About Me")}>About Me</p>
           </AnchorLink>
           {image === "About Me" ? <img src={abc} alt="" /> : <></>}
         </li> */}
-        <li>
-          <AnchorLink className="anchor-link" offset={50} href="#contact">
-            {" "}
-            <p onClick={() => setImage("Contact")}>Contact</p>{" "}
-          </AnchorLink>
+            <li>
+              <AnchorLink className="anchor-link" offset={50} href="#contact">
+                {" "}
+                <p onClick={() => setImage("Contact")}>Contact</p>{" "}
+              </AnchorLink>
 
-          {image === "Contact" ? <img src={abc} alt="" /> : <></>}
-        </li>
-        <li>
-          <AnchorLink className="anchor-link" offset={50} href="#Portfolio">
-            <p onClick={() => setImage("Portfolio")}>Projects</p>
-          </AnchorLink>
-          {image === "Portfolio" ? <img src={abc} alt="" /> : <></>}
-        </li>
-      </ul>
-      <div className="nav-connect">
-        <AnchorLink href="#contact" className="anchor-link" offset={50}>
-          Connect with me
-        </AnchorLink>
+              {image === "Contact" ? <img src={abc} alt="" /> : <></>}
+            </li>
+            <li>
+              <AnchorLink className="anchor-link" offset={50} href="#Portfolio">
+                <p onClick={() => setImage("Portfolio")}>Projects</p>
+              </AnchorLink>
+              {image === "Portfolio" ? <img src={abc} alt="" /> : <></>}
+            </li>
+          </ul>
+          <div className="nav-connect">
+            <AnchorLink href="#contact" className="anchor-link" offset={50}>
+              Connect with me
+            </AnchorLink>
+          </div>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 export default Navbar;
