030078945fa2559551796f8c0d7b144abec29389
Deva9754 <deva.sahu2001@gmail.com>
2024-06-11 01:33:43 +0530

    wrapped inside container

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,40 +1,43 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import "./Footer.css";
+import Container from "../container/Container";
 // import footer_logo from "../../assets/footer_logo.svg";
 // import user_icon from "../../assets/user_icon.svg";
 const Footer = () => {
   return (
-    <div>
-      <div className="footer">
-        <div className="footer-top">
-          <div className="footer-top-left">
-            {/* <img src="" alt="footer-logo" /> */}
-            <p>Front-end Developer with 2+ years of experience at wipro </p>
-          </div>
-          <div className="top-right">
-            {/* <div className="footer-email-input">
+    <Container>
+      <div>
+        <div className="footer">
+          <div className="footer-top">
+            <div className="footer-top-left">
+              {/* <img src="" alt="footer-logo" /> */}
+              <p>Front-end Developer with 2+ years of experience at wipro </p>
+            </div>
+            <div className="top-right">
+              {/* <div className="footer-email-input">
               <img src={user_icon} alt="user-icon" />
               <input type="email" placeholder="enter your email" />
             </div> */}
-            {/* <div className="footer-subscribe">Subscribe</div> */}
+              {/* <div className="footer-subscribe">Subscribe</div> */}
+            </div>
           </div>
-        </div>
-        <hr />
-        <div className="footer-bottom">
-          <p className="footer-bottom-left">
-            © 2024 Devashish sahu. All rights reserved
-          </p>
-          <div className="footer-bottom-right">
-            <p>Term of services</p>
-            <p> Privacy Policy</p>
-            <AnchorLink className="anchor-link" offset={50} href="#contact">
-              {" "}
-              <p>Connect with me</p>{" "}
-            </AnchorLink>
+          <hr />
+          <div className="footer-bottom">
+            <p className="footer-bottom-left">
+              © 2024 Devashish sahu. All rights reserved
+            </p>
+            <div className="footer-bottom-right">
+              <p>Term of services</p>
+              <p> Privacy Policy</p>
+              <AnchorLink className="anchor-link" offset={50} href="#contact">
+                {" "}
+                <p>Connect with me</p>{" "}
+              </AnchorLink>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 export default Footer;
