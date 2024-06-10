e3e4fdca4dababff87a4d102e01f4a3f7bf8d5d5
Deva9754 <deva.sahu2001@gmail.com>
2024-06-11 01:32:59 +0530

    wrapped inside container

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,6 +3,7 @@ import theme from "../../assets/theme_pattern.svg";
 import mail_icon from "../../assets/mail_icon.svg";
 import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
+import Container from "../container/Container";
 
 const Contact = () => {
   const onSubmit = async (event) => {
@@ -28,51 +29,53 @@ const Contact = () => {
     }
   };
   return (
-    <div id="contact" className="contact">
-      <div className="contact-title">
-        <h1>Get in touch</h1>
-        <img src={theme} alt="" />
-      </div>
-      <div className="contact-section">
-        <div className="contact-left">
-          <h1>Lets talk</h1>
-          <p>
-            I`m currently avaliable to take new oppourtunity, so feel free to
-            connect
-          </p>
-          <div className="contact-details">
-            <div className="contact-detail">
-              <img src={mail_icon} alt="" />
-              <p>deva.sahu2001@gmail.com</p>
-            </div>
-            <div className="contact-detail">
-              <img src={call_icon} alt="" />
-              <p>+917974075143</p>
-            </div>
+    <Container>
+      <div id="contact" className="contact">
+        <div className="contact-title">
+          <h1>Get in touch</h1>
+          <img src={theme} alt="" />
+        </div>
+        <div className="contact-section">
+          <div className="contact-left">
+            <h1>Lets talk</h1>
+            <p>
+              I`m currently avaliable to take new oppourtunity, so feel free to
+              connect
+            </p>
+            <div className="contact-details">
+              <div className="contact-detail">
+                <img src={mail_icon} alt="" />
+                <p>deva.sahu2001@gmail.com</p>
+              </div>
+              <div className="contact-detail">
+                <img src={call_icon} alt="" />
+                <p>+917974075143</p>
+              </div>
 
-            <div className="contact-detail">
-              <img src={location_icon} alt="" />
-              <p> Pune, Maharastra</p>
+              <div className="contact-detail">
+                <img src={location_icon} alt="" />
+                <p> Pune, Maharastra</p>
+              </div>
             </div>
           </div>
+          <form onSubmit={onSubmit} className="contact-right">
+            <label htmlFor="">Your Name</label>
+            <input type="text" placeholder="Enter your name" name="name" />
+            <label htmlFor="">Your Email</label>
+            <input type="email" placeholder="Enter your Email" name="email" />
+            <label htmlFor="">Your Message</label>
+            <textarea
+              name="message"
+              rows="8"
+              placeholder="Enter Your Message"
+            ></textarea>
+            <button className="contact-submit" type="submit">
+              Submit
+            </button>
+          </form>
         </div>
-        <form onSubmit={onSubmit} className="contact-right">
-          <label htmlFor="">Your Name</label>
-          <input type="text" placeholder="Enter your name" name="name" />
-          <label htmlFor="">Your Email</label>
-          <input type="email" placeholder="Enter your Email" name="email" />
-          <label htmlFor="">Your Message</label>
-          <textarea
-            name="message"
-            rows="8"
-            placeholder="Enter Your Message"
-          ></textarea>
-          <button className="contact-submit" type="submit">
-            Submit
-          </button>
-        </form>
       </div>
-    </div>
+    </Container>
   );
 };
 export default Contact;
