e63e5d05a04d5e634561b70003bbc96d90ce0d47
Deva9754 <deva.sahu2001@gmail.com>
2024-06-10 16:15:57 +0530

    projects added

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -1,68 +1,70 @@
+import "./Portfolio.css";
+
 const Portfolio = () => {
-  const Portfolios = [
+  const projects = [
     {
-      id: 1,
-      src: "https://static-00.iconduck.com/assets.00/messenger-icon-512x511-wsdhvs0c.png",
-      href: "https://github.com/Deva9754/Chatify/tree/master",
-      href1: "",
+      title: "Chatify",
+      imageSrc:
+        "https://static-00.iconduck.com/assets.00/messenger-icon-512x511-wsdhvs0c.png",
+      description:
+        "Developed a real-time chat application using Socket.IO for instant communication, featuring user authentication and message broadcasting between clients.",
+      skills: ["React", "socket.io", "Node"],
+      demo: "",
+      source: "https://github.com/Deva9754/Chatify/tree/master",
     },
     {
-      id: 2,
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_KbSaAU0XA2XTjF9pkm7-5R9j8KbWbuOgdg&s",
-      href: "https://github.com/Deva9754/Flipkart",
-      href1: "https://flipkartclone-devashish-sahus-projects.vercel.app/",
+      title: "Flipkart-clone",
+      imageSrc:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_KbSaAU0XA2XTjF9pkm7-5R9j8KbWbuOgdg&s",
+      description:
+        "Build a Flipkart clone, replicating the e-commerce platform's functionalities, including product listings, user authentication, and shopping cart management. ",
+      skills: ["React", "redux-toolkit", "MUI", "Sass"],
+      demo: "https://flipkartclone-devashish-sahus-projects.vercel.app/",
+      source: "https://github.com/Deva9754/Flipkart",
     },
     {
-      id: 3,
-      src: "https://a0.anyrgb.com/pngimg/906/338/windows-movie-maker-freemake-video-downloader-visual-effects-film-editing-video-editing-filmmaking-music-video-editing-coin-icon-design-thumbnail.png",
-      href: "https://github.com/Deva9754/Netflixgpt",
-      href1: "https://netflixgpt-omega.vercel.app/",
+      title: "Movie_Stream App",
+      imageSrc:
+        "https://a0.anyrgb.com/pngimg/906/338/windows-movie-maker-freemake-video-downloader-visual-effects-film-editing-video-editing-filmmaking-music-video-editing-coin-icon-design-thumbnail.png",
+      description:
+        "Designed a movie streaming application integrating GPT-powered chatbots for personalized recommendations, enhancing user engagement and satisfaction.",
+      skills: ["React", "MUI", "Redux-toolkit"],
+      demo: "https://netflixgpt-omega.vercel.app/",
+      source: "https://github.com/Deva9754/Netflixgpt",
     },
-    // {
-    //   id: 1,
-    //   src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_KbSaAU0XA2XTjF9pkm7-5R9j8KbWbuOgdg&s",
-    // },
   ];
-
   return (
-    <div
-      id="Portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
-    >
-      <div className=" max-w-sceen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className=" pb-8">
-          <p className=" text-4xl font-bold inline border-b-4 border-gray-500">
-            Portfolio
-          </p>
-          <p className=" py-6">Check out some out of my work right here</p>
-        </div>
-        <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {Portfolios.map(({ id, src, href, href1 }) => (
-            <div key={id} className=" shadow-md shadow-gray-600 rounded-xl">
-              <img
-                className=" px-36 rounded-lg duration-200 hover:scale-105"
-                src={src}
-                alt=""
-              />
-              <div className="flex items-center justify-center">
-                <a href={href1} target="_blank">
-                  <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Demo
-                  </button>{" "}
+    <div className="container" id="projects">
+      <h2 className="text-4xl font-bold inline border-b-4 border-gray-500">
+        Projects
+      </h2>
+      <div className="projects">
+        {projects.map((project, id) => {
+          return (
+            <div key={id} className="container-card">
+              <img src={project.imageSrc} alt="img" className="image" />
+              <h3 className="title">{project.title}</h3>
+              <p className="description">{project.description}</p>
+              <ul className="skills">
+                {project.skills.map((skill, id) => {
+                  return (
+                    <li key={id} className="skill">
+                      {skill}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="links">
+                <a href={project.demo} className="link">
+                  Demo
                 </a>
-
-                <a href={href} target="_blank">
-                  <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Code
-                  </button>
+                <a href={project.source} className="link">
+                  Source
                 </a>
               </div>
             </div>
-          ))}
-          <a href="https://assignment-01-pied.vercel.app/" target="_blank">
-            <p className=" ">For more...</p>
-          </a>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
