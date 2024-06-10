a33b82ffd79161cf5ce3fd5dffe5f582f57e599f
Deva9754 <deva.sahu2001@gmail.com>
2024-06-10 16:17:17 +0530

    openpdf added

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,14 @@
-import "./profile.css";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-
+import "./profile.css";
 const Profile = () => {
+  const openPDF = () => {
+    // Assuming your PDF file is named "example.pdf" and is located in the "public" folder
+    var pdfPath = "/Devashish_sahu_Resume_frontend (2).pdf";
+
+    // Open the PDF in a new tab
+    window.open(pdfPath, "_blank");
+  };
+
   return (
     <div className="profile">
       <img
@@ -10,12 +17,18 @@ const Profile = () => {
       />
       <h1>
         {" "}
-        <span>I`m Devashish Sahu</span> Frontend developer based in India.
+        <span>I`m Devashish Sahu</span> Frontend developer.
       </h1>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod
-        voluptates eveniet soluta ut velit a assumenda voluptatibus dolorum
-        laboriosam sapiente!
+        As a dedicated Frontend Developer at Wipro since 2022, I specialize in
+        crafting immersive user experiences using ReactJS, Redux, and
+        JavaScript. I pay meticulous attention to detail in HTML and CSS, and
+        leverage tools like Tailwind CSS and Material-UI for optimal performance
+        and aesthetic appeal. I also excel in data visualization with D3.js and
+        ensure seamless component integration through Storybook.{" "}
+        {/*  My tenure at
+        Wipro has empowered me to deliver innovative solutions while honing my
+        expertise in frontend development.  */}
       </p>
       <div className="profile-action">
         <div className="profile-connect">
@@ -24,9 +37,12 @@ const Profile = () => {
             Connect with me{" "}
           </AnchorLink>
         </div>
-        <div className="profile-resume">Resume</div>
+        <div className="profile-resume" onClick={openPDF}>
+          Resume
+        </div>
       </div>
     </div>
   );
 };
+
 export default Profile;
