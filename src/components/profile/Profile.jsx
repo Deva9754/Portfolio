901e2d9fdc68c0699529f543bb8d61fd17db93ad
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-12-05 20:40:35 +0530

    fix: profile company name

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -32,14 +32,15 @@ const Profile = () => {
         <TextSlider />
         <p>
           I’m a skilled Frontend Developer with over 3.6+ years of hands-on
-          experience, currently working with Infinit Locus. I’ve contributed to
-          the development of SuperTails, a product-focused application, where I
-          consistently delivered high-quality and performant frontend solutions.
-          My expertise includes modern technologies such as ReactJS, Redux,
-          JavaScript, HTML/CSS, and D3.js, with a strong emphasis on crafting
-          responsive, user-friendly interfaces. I’m also proficient in using
-          design systems and utility-first frameworks like Tailwind CSS and
-          Material-UI to build scalable and maintainable user interfaces.
+          experience, currently working with Empyra Software Solutions. I’ve
+          contributed to the development of SuperTails, a product-focused
+          application, where I consistently delivered high-quality and
+          performant frontend solutions. My expertise includes modern
+          technologies such as ReactJS, Redux, JavaScript, HTML/CSS, and D3.js,
+          with a strong emphasis on crafting responsive, user-friendly
+          interfaces. I’m also proficient in using design systems and
+          utility-first frameworks like Tailwind CSS and Material-UI to build
+          scalable and maintainable user interfaces.
         </p>
         <div className="profile-action">
           <AnchorLink href="#contact" className="anchor-link" offset={50}>
